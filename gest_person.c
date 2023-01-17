91ce08ed0ec6fc6501c052bb25591740bdebbf00
psoares-eng <121614200+psoares-eng@users.noreply.github.com>
2023-01-17 01:08:33 +0000

    Gestor de Pessoas
    Limpeza

--- gest_person.c
+++ gest_person.c
@@ -59,16 +59,14 @@ void manage_persons(FILE *fp, LIST_PERSONS* list_persons){
 // Return boolean
 // REF read_country_from_txt_file()
 int read_person_from_txt_file(FILE *fp, PERSON *person) {
-    char string[500]; // VALIDAR valor
-    int n_fields = fgets(string, 500, fp);
-    char *campo[100] = { NULL }; // VALIDAR valor
+    char line[MAX_LINE];
+    char *n_fields = fgets(line, MAX_LINE, fp);
+    char *campo[MAX_ADDRESS_SIZE] = { NULL };
+
     int count = 0;
-
-    if( n_fields!=0 ){ // VALIDAR // Verificar se n_fields não null
-        //printf("\nFIELDS #%d\n", (n_fields));
-
-        strreplace(string, "\t\t", "\tNULL\t"); // substitui NULL por "NULL"
-        tokenize(string, "\t", campo, &count);
+    if( n_fields != NULL ) { // VALIDAR // Verificar se n_fields não null
+        strreplace(line, "\t\t", "\tNULL\t"); // substitui NULL por "NULL"
+        tokenize(line, "\t", campo, &count);
 
         person->id = atoi(campo[0]); // VALIDAR uso de atoi
         strcpy( person->first_name, campo[1] );
@@ -99,34 +97,32 @@ int import_persons(LIST_PERSONS* list_persons) {
     //list_persons.count = 0;   // Nao ha pessoas na lista
 
     // Importar os dados do ficheiro para a lista de countries
-    FILE *fp;         // Ficheiro para importar os dados.
+    FILE* fp_csv;         // Ficheiro para importar os dados.
 
-    char info[MAX_STRING];
     char filepath[MAX_STRING];
     //sprintf(filepath, "%s/%s", "datasets", "all-persons.csv");
     // Remover cabecalhos do ficheiro e campos para simplificar a importacao
-    sprintf(filepath, "%s/%s", "datasets", "persons.csv");
-    sprintf(info, "\nImportar o ficheiro \"%s\" com as pessoas. Prima <ENTER>...\n", filepath);
-    wait(info);
+    cls();
+    sprintf( filepath, "%s/%s", "datasets", "persons.csv" );
+    printf( "\nImportar o ficheiro \"%s\" com os registos\n", filepath );
+    //wait("Prima <ENTER> para continuar...\n");
 
-    if ((fp=fopen(filepath, "rt"))==NULL)
+    if ( ( fp_csv = fopen( filepath, "rt" ) ) == NULL )
     {
         wait("Problemas na abertura do ficheiro de pessoas.");
         return 1;
     }
 
-    //int pid = 0;
-    while (read_person_from_txt_file(fp, &list_persons->person[list_persons->count])) // Enquanto conseguir ler um registo
+    while ( read_person_from_txt_file( fp_csv, &list_persons->person[list_persons->count] ) ) // Enquanto conseguir ler um registo
     {
         ++list_persons->count;
     }
 
     // VALIDAR // REMOVER // PAUSAR PARA VERIFICACAO DOS DADOS APENAS
-    //sprintf(info, "\nTotal de registos csv = %d.\n", pid);
     printf("\nTotal de registos csv = %d.\n", list_persons->count);
     wait("Prima <ENTER> para continuar...\n");
 
-    fclose(fp);
+    fclose( fp_csv );
     return(0);
 }
 
@@ -137,7 +133,7 @@ int save_persons(FILE* fp, LIST_PERSONS* list_persons) {
     if (n_recs!=list_persons->count)
     {
         wait("Problemas na gravacao dos registos no ficheiro binario. Prima <ENTER>...");
-        return;
+        return 1;
     }
     else
     {
@@ -146,31 +142,44 @@ int save_persons(FILE* fp, LIST_PERSONS* list_persons) {
         printf("\nTotal de registos gravados com sucesso = %d.\n", n_recs);
         wait("Prima <ENTER> para continuar...\n");
     }
-    return(0);
+    return 0;
 }
 
 // VALIDAR // Listar Pessoas
 // Exibir resultados de melhor forma
 int show_persons(LIST_PERSONS *list_persons) {
-    printf("LISTA DE PESSOAS\n\n");
-    //printf("A ID  Primeiro Nome\t\tApelido\t\tNome Completo\t\tMorada\t\tEmail\t\tNascimento\t\tGenero\t\tCódigo Postal\t\tPaís\t\tDepartmento\n\n");
-    printf("A ID   Nome\t\tApelido\t\tMorada\t\tNascimento\t\tGenero\t\tCP\t\tPaís\t\tDepartmento\n\n");
+    cls();
+    printf("LISTA DE PESSOAS\n");
+    printf("\nExistem %d pessoas em memoria\n\n", list_persons->count);
+    printf("A ID  Nome            APELIDO              Morada                     Nasc.     Genero CP     Departmento    \n\n");
     for (int i = 0; i < list_persons->count; i++) {
         //show_person_vertical(&list_persons->person[i]);
+
+        //printf("%1d %3d %15s %20s %25s %9s %6s %6s %15s\n",
         show_person_horizontal(&list_persons->person[i]);
         //printf("\n");
     }
-    printf("\nExistem %d pessoas em memoria\n", list_persons->count);
+    //printf("\nExistem %d pessoas em memoria\n", list_persons->count);
+    printf("\n\n");
     wait("Prima <ENTER>...\n");
-    return(0);
+    return 0;
 }
 
 // VALIDAR // Ordenar Lista
 int sort_persons(LIST_PERSONS* list_persons) {
-    wait("\n\nOrdenar lista de pessoas pelo nome do pais. Prima <ENTER>...");
+    printf("\n\nOrdenar lista de pessoas pelo Nome");
     qsort(list_persons->person, (long) list_persons->count, sizeof(PERSON), comp);
     wait("Prima <ENTER> para continuar...\n");
-    return(0);
+    return 0;
+}
+
+// VALIDAR // Comparar Nomes
+int comp(const void*ptr1, const void* ptr2) {
+  PERSON *p1 = (PERSON *) ptr1;
+  PERSON *p2 = (PERSON *) ptr2;
+
+  // comparar primeiro nome
+  return strcmp(p1->first_name, p2->first_name);
 }
 
 // INSERIR Processa um novo registo de pessoa
@@ -199,7 +208,6 @@ void remove_person(FILE *fp, LIST_PERSONS *list_persons) {
   // TODO // REMOVE Processa um novo registo de pessoa
 }
 
-//
 // Grava um registo de pessoa no ficheiro.
 // O processamento difere caso a operacao seja uma insercao ou alteracao
 // Devolve um valor boolean indicando se a operacao foi realizada com uscesso
@@ -211,13 +219,4 @@ int save_person(FILE *fp, LIST_PERSONS *list_persons, PERSON *p, char operation)
   // TODO // Grava um registo de pessoa no ficheiro.
 
   return 1;
-}
-
-// VALIDAR // Comparar Nomes
-int comp(const void*ptr1, const void* ptr2) {
-  PERSON *p1 = (PERSON *) ptr1;
-  PERSON *p2 = (PERSON *) ptr2;
-
-  // comparar primeiro nome
-  return strcmp(p1->first_name, p2->first_name);
 }
--- includes/gest_person.h
+++ includes/gest_person.h
@@ -10,9 +10,13 @@ void manage_persons(FILE *fp, LIST_PERSONS* list_persons);
 void insert_person(FILE *fp, LIST_PERSONS *list_persons);
 void update_person(FILE *fp, LIST_PERSONS *list_persons);
 void remove_person(FILE *fp, LIST_PERSONS *list_persons);
-
 int save_person(FILE *fp, LIST_PERSONS *list_persons, PERSON *p, char operation);
 
+// VALIDAR
+int import_persons(LIST_PERSONS* list_persons);
+int save_persons(FILE* fp, LIST_PERSONS* list_persons);
+int show_persons(LIST_PERSONS *list_persons);
+int sort_persons(LIST_PERSONS* list_persons);
 int comp(const void*ptr1, const void* ptr2);
 
 #endif // __GEST_PERSON__
